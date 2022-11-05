7ed58a1f4b97c7f84a22ff841473ac6382ccaf1f
Ceejay Pimentel <ceejaypimentel@hotmail.ca>
2022-11-05 14:21:55 -0500

    Final font changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ["./**/*.html"],
-  },
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],

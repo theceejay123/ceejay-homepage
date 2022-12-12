3dbdd0c59d4650bccd2b0854ed741d0e07a30a12
Ceejay Pimentel <ceejaypimentel@hotmail.ca>
2022-12-12 07:44:34 -0600

    WIP aboutme page

--- components/About/About.component.tsx
+++ components/About/About.component.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  const aboutMe = "";
+
+  return (
+    <div className="h-screen max-w-6xl mx-auto px-4">
+      <div className="w-full h-full">
+        <h2>About Me</h2>
+        <p>{aboutMe}</p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
